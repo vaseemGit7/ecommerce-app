b9af589c126f8bb22959f994012a080a2493e85a
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-04-12 13:46:43 +0530

    Add form elements and refactor code

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,24 +1,39 @@
+import { useRef } from "react";
 import { credentialsDb } from "../database/credentials";
 
 const LoginPage = () => {
-  const emailIdInput = prompt("Enter your emailId");
-  const passwordInput = prompt("Enter your password");
-  let validUser = false;
+  const emailIdInput = useRef(null);
+  const passwordInput = useRef(null);
+  const loginForm = useRef(null);
 
-  const checkValidUser = () => {
-    credentialsDb.map(
-      (user) =>
-        (validUser =
-          user.emailId === emailIdInput && user.password === passwordInput
-            ? true
-            : false)
-    );
+  const checkValidUser = (e) => {
+    e.preventDefault();
+    let validUser = false;
+
+    credentialsDb.forEach((user) => {
+      if (
+        user.emailId === emailIdInput.current.value &&
+        user.password === passwordInput.current.value
+      ) {
+        validUser = true;
+        return;
+      }
+    });
     validUser ? console.log("Login success") : console.log("Login failed");
+    loginForm.current.reset();
   };
 
-  checkValidUser();
-
-  return <></>;
+  return (
+    <div id="loginContainer">
+      <form ref={loginForm} onSubmit={(e) => checkValidUser(e)} id="loginForm">
+        <label className="inputLabel">EmailId</label>
+        <input ref={emailIdInput} type="email"></input>
+        <label className="inputLabel">Password</label>
+        <input ref={passwordInput} type="password"></input>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
