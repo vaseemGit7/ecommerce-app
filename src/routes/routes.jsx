388a644ac4175c7fe459debaf79294508e1ec63d
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-04-16 00:32:48 +0530

    Implement Suspense Component from react

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,7 @@
+import { lazy, Suspense } from "react";
 import LoginPage from "../components/LoginPage";
-import Dashboard from "../components/Dashboard";
+import Loading from "../components/Loading";
+const LazyDashBoard = lazy(() => delayRoute(import("../components/Dashboard")));
 
 const routes = [
   {
@@ -10,8 +12,18 @@ const routes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    element: <Dashboard />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <LazyDashBoard />
+      </Suspense>
+    ),
   },
 ];
 
 export default routes;
+
+function delayRoute(promise) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 3500);
+  }).then(() => promise);
+}
