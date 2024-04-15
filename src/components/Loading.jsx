c490454c3329a703f5f97083cbf4209882d81c26
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-04-16 00:31:55 +0530

    Create Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,17 @@
+const Loading = () => {
+  console.log("Fallback is called");
+  return (
+    <>
+      <div className="flex items-center px-4 py-2 rounded-lg bg-violet-900 gap-2">
+        <div className="w-6 h-6 border-4 border-white border-b-violet-900 rounded-full inline-block animate-spin">
+          {" "}
+        </div>
+        <span className="text-md font-semibold text-neutral-50">
+          Loading...
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
