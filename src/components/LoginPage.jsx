0c29d282e195166493596d138c68584eb4ee82e5
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-04-16 18:19:25 +0530

    Update layout

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -68,67 +68,69 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 p-2 bg-neutral-200/50 bg-opa rounded-xl drop-shadow-lg">
-      <div
-        className="m-3 bg-cover bg-center rounded-lg"
-        style={{ backgroundImage: "url('./src/assets/login-bg-image.jpeg')" }}
-      ></div>
-      <div className=" mx-12 my-24 py-16 " id="loginContainer">
-        <form
-          className="flex flex-col items-center gap-14"
-          ref={loginForm}
-          onSubmit={(e) => checkValidUser(e)}
-          id="loginForm"
-        >
-          <div className="relative flex flex-col gap-1">
-            <label className="text-xs font-semibold text-neutral-600">
-              EMAIL
-            </label>
-            <input
-              className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
-              ref={emailIdInput}
-              type="email"
-              onBlur={() => {
-                checkEmailValidity();
-              }}
-              onFocus={() => {
-                setEmailError("");
-              }}
-            ></input>
-            {emailError && (
-              <p className="absolute -bottom-5 text-sm font-medium text-red-600">
-                {emailError}
-              </p>
-            )}
-          </div>
-          <div className="relative flex flex-col gap-1">
-            <label className="text-xs font-semibold text-neutral-600">
-              PASSWORD
-            </label>
-            <input
-              className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
-              ref={passwordInput}
-              type="password"
-              onBlur={() => {
-                checkPasswordValidity();
-              }}
-              onFocus={() => {
-                setPasswordError("");
-              }}
-            ></input>
-            {passwordError && (
-              <p className="absolute -bottom-5 text-sm font-medium text-red-600">
-                {passwordError}
-              </p>
-            )}
-          </div>
-          <button
-            className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
-            type="submit"
+    <div className="h-full grid place-items-center">
+      <div className="grid grid-cols-2 p-2 bg-neutral-200/50 bg-opa rounded-xl drop-shadow-lg">
+        <div
+          className="m-3 bg-cover bg-center rounded-lg"
+          style={{ backgroundImage: "url('./src/assets/login-bg-image.jpeg')" }}
+        ></div>
+        <div className=" mx-12 my-24 py-16 " id="loginContainer">
+          <form
+            className="flex flex-col items-center gap-14"
+            ref={loginForm}
+            onSubmit={(e) => checkValidUser(e)}
+            id="loginForm"
           >
-            SIGN IN
-          </button>
-        </form>
+            <div className="relative flex flex-col gap-1">
+              <label className="text-xs font-semibold text-neutral-600">
+                EMAIL
+              </label>
+              <input
+                className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
+                ref={emailIdInput}
+                type="email"
+                onBlur={() => {
+                  checkEmailValidity();
+                }}
+                onFocus={() => {
+                  setEmailError("");
+                }}
+              ></input>
+              {emailError && (
+                <p className="absolute -bottom-5 text-sm font-medium text-red-600">
+                  {emailError}
+                </p>
+              )}
+            </div>
+            <div className="relative flex flex-col gap-1">
+              <label className="text-xs font-semibold text-neutral-600">
+                PASSWORD
+              </label>
+              <input
+                className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
+                ref={passwordInput}
+                type="password"
+                onBlur={() => {
+                  checkPasswordValidity();
+                }}
+                onFocus={() => {
+                  setPasswordError("");
+                }}
+              ></input>
+              {passwordError && (
+                <p className="absolute -bottom-5 text-sm font-medium text-red-600">
+                  {passwordError}
+                </p>
+              )}
+            </div>
+            <button
+              className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
+              type="submit"
+            >
+              SIGN IN
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
