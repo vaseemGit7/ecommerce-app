35fecacf0cdddd5a748a66a35719eea1f9ef5192
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-20 15:25:04 +0530

    Create Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import DoughnutChart from "./DoughnutChart";
+
+const Home = () => {
+  return (
+    <>
+      <div className=" h-96 w-96 bg-neutral-50 rounded-lg drop-shadow-md">
+        <DoughnutChart />
+      </div>
+    </>
+  );
+};
+
+export default Home;
