3a771009079617a64c74561bb7ed3d05d63763ba
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-21 15:20:57 +0530

    Create a pesudo users database

---
+++ src/database/usersData.js
@@ -0,0 +1,16 @@
+const usersDb = [
+  {
+    id: 1,
+    userName: "Mr.abcd",
+    emailId: "abcd1234@gmail.com",
+    dob: "30.05.2000",
+  },
+  {
+    id: 2,
+    userName: "Vaseem",
+    emailId: "vaseem@gmail.com",
+    dob: "01.01.2001",
+  },
+];
+
+export default usersDb;
