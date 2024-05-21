2d0c7728acae984e11983d371597a566a5237dd7
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-21 15:55:28 +0530

    Update Profile to render user details

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,23 @@
 import { useSelector } from "react-redux";
+import usersDb from "../database/usersData";
 
 const Profile = () => {
   const userDetails = useSelector((state) => state.userReducer);
 
-  console.log("the loggined user id is", userDetails);
+  const currentUser = usersDb.find((user) => user.id === userDetails.id);
 
-  return <></>;
+  return (
+    <div className="grid place-items-center">
+      <div className="flex gap-5 p-5 font-medium text-neutral-900 text-base bg-neutral-50 rounded">
+        <div className="rounded-full h-32 w-32 bg-neutral-500"></div>
+        <div className="flex flex-col justify-center">
+          <p>{currentUser.userName}</p>
+          <p>{currentUser.emailId}</p>
+          <p>{currentUser.dob}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
