e1628da8aa3a04df8ad4e48ffc1b9859b0c77169
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-29 15:04:11 +0530

    Implement total calculation functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Cart = () => {
   const cartProduct = useSelector((state) => state.cartReducer);
+  const [orderValue, setOrderValue] = useState(0);
+  const [deliveryCharge, setDeliveryCharge] = useState(30);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const calculateOrderValue = () => {
+      const value = cartProduct.reduce(
+        (acc, product) => acc + product.price,
+        0
+      );
+      setOrderValue(value);
+    };
+
+    const calculateTotalPrice = () => {
+      const total = orderValue + deliveryCharge;
+      setTotalPrice(total);
+    };
+
+    calculateOrderValue();
+    calculateTotalPrice();
+  }, [cartProduct, deliveryCharge, orderValue]);
+
   console.log(cartProduct);
   return (
-    <div className="grid items-start justify-center mx-28">
+    <div className="mx-28">
       <div className="grid grid-cols-[3fr_2fr] ">
         <div className="flex flex-col p-4 gap-3">
           <div>
@@ -29,11 +52,35 @@ const Cart = () => {
                 </div>
               ))
             ) : (
-              <p>Your bag is empty</p>
+              <p className="text-2xl font-semibold align-middle text-center p-7 text-neutral-700">
+                Your bag is empty
+              </p>
             )}
           </div>
         </div>
-        <div className="border-2 border-green-500"></div>
+        <div className="mt-2 p-3">
+          <div className="flex flex-col bg-slate-100 p-3 rounded-md">
+            <div className="flex justify-between pb-3 border-b-2 border-neutral-500">
+              <div>
+                <p className=" text-base font-normal">Order value</p>
+                <p className=" text-base font-normal">Delivery charges</p>
+              </div>
+              <div>
+                <p className=" text-base font-normal">Rs. {orderValue}</p>
+                <p className=" text-base font-normal">
+                  {deliveryCharge === 0 ? "Free" : `Rs. ${deliveryCharge}`}
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-between items-center pt-3">
+              <p className="text-xl font-medium">Total charges</p>
+              <p className="text-xl font-medium">Rs. {totalPrice}</p>
+            </div>
+            <button className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded hover:drop-shadow-lg">
+              Place Order
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
