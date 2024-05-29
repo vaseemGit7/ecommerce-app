6efab4343298be1ef9604238cfa725609191b513
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-29 11:01:25 +0530

    Update code with cart actions

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -1,6 +1,12 @@
 import { SET_USER } from "../reducers/userReducer";
+import { ADD_PRODUCT_CART } from "../reducers/cartReducer";
 
 export const setUser = (userId) => ({
   type: SET_USER,
   payload: userId,
+});
+
+export const addProductToCart = (product) => ({
+  type: ADD_PRODUCT_CART,
+  payload: product,
 });
