0a4c42d1c2bdf3127d52565605ff8b58af1e1efb
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-29 15:27:43 +0530

    Update actions to remove product from cart

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -1,5 +1,6 @@
 import { SET_USER } from "../reducers/userReducer";
 import { ADD_PRODUCT_CART } from "../reducers/cartReducer";
+import { REMOVE_PRODUCT_CART } from "../reducers/cartReducer";
 
 export const setUser = (userId) => ({
   type: SET_USER,
@@ -9,4 +10,9 @@ export const setUser = (userId) => ({
 export const addProductToCart = (product) => ({
   type: ADD_PRODUCT_CART,
   payload: product,
+});
+
+export const removeProductFromCart = (id) => ({
+  type: REMOVE_PRODUCT_CART,
+  payload: id,
 });
