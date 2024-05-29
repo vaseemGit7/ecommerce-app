c0ae10b57ce0a099ba394b0bb8b0fe8eafe88e77
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-29 16:45:56 +0530

    Implement loading in ProductDetail component

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 import { getProductDetail } from "../api/API";
 import { useDispatch } from "react-redux";
 import { addProductToCart } from "../actions/userActions";
+import DetailLoading from "./DetailLoading";
 
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const hasFetched = useRef(false);
   const dispatch = useDispatch();
@@ -17,6 +19,7 @@ const ProductDetail = () => {
       try {
         const data = await getProductDetail(id);
         setProduct(data.product);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -45,33 +48,37 @@ const ProductDetail = () => {
   console.log(product);
 
   return (
-    <div className="grid items-start justify-center mx-28">
-      <div className="grid grid-cols-2 p-7 justify-items-center bg-slate-100  text-neutral-800 rounded">
-        {product && (
-          <>
-            <div className="w-5/6">
-              <img
-                className="rounded"
-                src={product.articlesList[0].galleryDetails[0].baseUrl}
-              />
+    <div
+      className={`grid items-start mx-28 ${
+        loading ? "h-screen" : "justify-center"
+      }`}
+    >
+      {loading ? (
+        <DetailLoading />
+      ) : (
+        <div className="grid grid-cols-2 p-7 justify-items-center bg-slate-100  text-neutral-800 rounded">
+          <div className="w-5/6">
+            <img
+              className="rounded"
+              src={product.articlesList[0].galleryDetails[0].baseUrl}
+            />
+          </div>
+          <div className="p-5 flex flex-col gap-2 ">
+            <p className="text-xl font-semibold">{product.name}</p>
+            <p className="text-lg font-medium">₹{product.whitePrice.price}</p>
+            <button
+              className="py-2 px-3 w-3/5 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded"
+              onClick={addProduct}
+            >
+              Add
+            </button>
+            <div>
+              <p className="text-lg font-medium">DESCRIPTION</p>
+              <p className="text-sm font-normal">{product.description}</p>
             </div>
-            <div className="p-5 flex flex-col gap-2 ">
-              <p className="text-xl font-semibold">{product.name}</p>
-              <p className="text-lg font-medium">₹{product.whitePrice.price}</p>
-              <button
-                className="py-2 px-3 w-3/5 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded"
-                onClick={addProduct}
-              >
-                Add
-              </button>
-              <div>
-                <p className="text-lg font-medium">DESCRIPTION</p>
-                <p className="text-sm font-normal">{product.description}</p>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
