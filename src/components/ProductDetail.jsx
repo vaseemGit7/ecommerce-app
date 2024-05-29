e66dfd89fdbb818793b7c615599ad8beba497db7
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-29 10:45:00 +0530

    Update code

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { getProductDetail } from "../api/API";
 
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
   const { id } = useParams();
+  const hasFetched = useRef(false);
 
   useEffect(() => {
     const getProductById = async () => {
@@ -17,28 +18,35 @@ const ProductDetail = () => {
       }
     };
 
-    getProductById();
+    if (!hasFetched.current) {
+      getProductById(id);
+      hasFetched.current = true;
+    }
   }, [id]);
 
   console.log(product);
 
   return (
     <div className="grid items-start justify-center mx-28">
-      <div className="grid grid-cols-2 p-2 justify-items-center bg-slate-50 border-2 border-purple-700 text-neutral-900 rounded">
+      <div className="grid grid-cols-2 p-7 justify-items-center bg-slate-100  text-neutral-800 rounded">
         {product && (
           <>
             <div className="w-5/6">
               <img
-                className="border-2  border-green-500"
+                className="rounded"
                 src={product.articlesList[0].galleryDetails[0].baseUrl}
               />
             </div>
-            <div className="p-5 flex flex-col gap-2 border-2 border-green-500">
+            <div className="p-5 flex flex-col gap-2 ">
               <p className="text-xl font-semibold">{product.name}</p>
-              <p className="text-lg font-medium">
-                $ {product.whitePrice.price}
-              </p>
-              <p className="text-base font-normal">{product.description}</p>
+              <p className="text-lg font-medium">₹{product.whitePrice.price}</p>
+              <button className="py-2 px-3 w-3/5 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded">
+                Add
+              </button>
+              <div>
+                <p className="text-lg font-medium">DESCRIPTION</p>
+                <p className="text-sm font-normal">{product.description}</p>
+              </div>
             </div>
           </>
         )}
