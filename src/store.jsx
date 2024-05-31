7ff62b7b708e24e61caf18076ea299f1864be8d8
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-31 11:13:41 +0530

    Add paramsReducer to store

--- src/store.jsx
+++ src/store.jsx
@@ -2,10 +2,12 @@ import { createStore } from "redux";
 import { combineReducers } from "redux";
 import userReducer from "./reducers/userReducer";
 import cartReducer from "./reducers/cartReducer";
+import paramsReducer from "./reducers/paramsReducer";
 
 const combinedReducer = combineReducers({
   userReducer: userReducer,
   cartReducer: cartReducer,
+  paramsReducer: paramsReducer,
 });
 
 const store = createStore(combinedReducer);
