d770251ebebae1157e646823850cb0565bad23fc
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-31 15:11:44 +0530

    Fix mistakes

--- src/actions/dataActions.jsx
+++ src/actions/dataActions.jsx
@@ -1,4 +1,4 @@
-import { SET_RESULT_DATA } from "../reducers/paramsReducer";
+import { SET_RESULT_DATA } from "../reducers/dataReducer";
 
 export const setResultData = (data) => ({
   type: SET_RESULT_DATA,
