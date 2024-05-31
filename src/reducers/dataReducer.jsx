6a407e1626f343a05f202d22264b5c41d8d44ee4
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-31 15:38:15 +0530

    Refactor data reducer

--- src/reducers/dataReducer.jsx
+++ src/reducers/dataReducer.jsx
@@ -1,13 +1,11 @@
 export const SET_RESULT_DATA = "SET_RESULT_DATA";
 
-const initialState = {
-  resultData: {},
-};
+const initialState = {};
 
 const dataReducer = (state = initialState, actions) => {
   switch (actions.type) {
     case SET_RESULT_DATA:
-      return { ...state, resultData: actions.payload };
+      return actions.payload;
     default:
       return state;
   }
