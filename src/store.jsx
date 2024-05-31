50e1142e5c9cf7bcead60c36fca36659d07f4593
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-31 14:48:01 +0530

    Add dataReducer to store

--- src/store.jsx
+++ src/store.jsx
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import userReducer from "./reducers/userReducer";
 import cartReducer from "./reducers/cartReducer";
 import paramsReducer from "./reducers/paramsReducer";
+import dataReducer from "./reducers/dataReducer";
 
 const combinedReducer = combineReducers({
   userReducer: userReducer,
   cartReducer: cartReducer,
   paramsReducer: paramsReducer,
+  dataReducer: dataReducer,
 });
 
 const store = createStore(combinedReducer);
