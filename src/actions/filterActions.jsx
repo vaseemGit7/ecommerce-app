98fb6da6d4f3f2c13ed8af23b17e5c80751517a6
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-31 23:33:11 +0530

    Update filterActions to handle setParam functionality

--- src/actions/filterActions.jsx
+++ src/actions/filterActions.jsx
@@ -1,6 +1,11 @@
-import { SET_SEARCH_QUERY } from "../reducers/paramsReducer";
+import { SET_PARAM, SET_SEARCH_QUERY } from "../reducers/paramsReducer";
 
 export const setSearchQuery = (query) => ({
   type: SET_SEARCH_QUERY,
   payload: query,
+});
+
+export const setParam = (key, value) => ({
+  type: SET_PARAM,
+  payload: { key, value },
 });
