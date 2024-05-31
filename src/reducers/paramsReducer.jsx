8e9a1369248ac2bc42619852afcf04b2626eb9ba
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-05-31 23:31:33 +0530

    Update paramsReducer to handle parameter values

--- src/reducers/paramsReducer.jsx
+++ src/reducers/paramsReducer.jsx
@@ -1,13 +1,17 @@
 export const SET_SEARCH_QUERY = "SET_SEARCH_QUERY";
+export const SET_PARAM = "SET_PARAM";
 
 const initialState = {
   searchQuery: "",
+  sortBy: "stock",
 };
 
 const paramsReducer = (state = initialState, actions) => {
   switch (actions.type) {
     case SET_SEARCH_QUERY:
       return { ...state, searchQuery: actions.payload };
+    case SET_PARAM:
+      return { ...state, [actions.payload.key]: actions.payload.value };
     default:
       return state;
   }
