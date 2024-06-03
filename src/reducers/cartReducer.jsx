6ce191ee2cdfdcb3ebc11f6a8321287c8f946dab
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-03 11:51:19 +0530

    Update cartReducer to handle product quantiy

--- src/reducers/cartReducer.jsx
+++ src/reducers/cartReducer.jsx
@@ -1,5 +1,7 @@
 export const ADD_PRODUCT_CART = "ADD_PRODUCT_CART";
 export const REMOVE_PRODUCT_CART = "REMOVE_PRODUCT_CART";
+export const INCREASE_PRODUCT_QUANTITY = "INCREASE_PRODUCT_QUANTITY";
+export const DECREASE_PRODUCT_QUANTITY = "DECREASE_PRODUCT_QUANTITY";
 
 const initialState = [];
 
@@ -7,6 +9,18 @@ const cartReducer = (state = initialState, actions) => {
   switch (actions.type) {
     case ADD_PRODUCT_CART:
       return [...state, actions.payload];
+    case INCREASE_PRODUCT_QUANTITY:
+      return state.map((product) =>
+        product.id === actions.payload
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
+      );
+    case DECREASE_PRODUCT_QUANTITY:
+      return state.map((product) =>
+        product.id === actions.payload
+          ? { ...product, quantity: Math.max(product.quantity - 1, 0) }
+          : product
+      );
     case REMOVE_PRODUCT_CART:
       return state.filter((product) => product.id !== actions.payload);
     default:
