0044b0dc84fd61c175998f5ad4355eb97d7b685f
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-03 11:52:15 +0530

    Update actions for product increment and decrement

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -1,5 +1,9 @@
 import { SET_USER } from "../reducers/userReducer";
-import { ADD_PRODUCT_CART } from "../reducers/cartReducer";
+import {
+  ADD_PRODUCT_CART,
+  DECREASE_PRODUCT_QUANTITY,
+  INCREASE_PRODUCT_QUANTITY,
+} from "../reducers/cartReducer";
 import { REMOVE_PRODUCT_CART } from "../reducers/cartReducer";
 
 export const setUser = (userId) => ({
@@ -10,6 +14,16 @@ export const setUser = (userId) => ({
 export const addProductToCart = (product) => ({
   type: ADD_PRODUCT_CART,
   payload: product,
+});
+
+export const increaseProductQuantity = (id) => ({
+  type: INCREASE_PRODUCT_QUANTITY,
+  payload: id,
+});
+
+export const decreaseProductQuantity = (id) => ({
+  type: DECREASE_PRODUCT_QUANTITY,
+  payload: id,
 });
 
 export const removeProductFromCart = (id) => ({
