f332712ca77b99a8b704eb080a780461755e3cff
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-03 22:36:51 +0530

    Create Checkout component

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,16 @@
+const Checkout = () => {
+  return (
+    <div className="mx-28">
+      <div className="grid grid-cols-[3fr_2fr] ">
+        <div className="flex flex-col p-4 gap-3">
+          <div>
+            <p className="text-2xl font-bold"> Checkout</p>
+          </div>
+        </div>
+        <div className="mt-2 p-3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
