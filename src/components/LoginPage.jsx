8ae8b185041a6afd72f0d4c3e177d20947bf1a63
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-03 19:46:46 +0530

    Refactor login with Formik and Yup

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,5 +1,4 @@
-import { useRef, useState } from "react";
-import { credentialsDb } from "../database/credentials";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { Formik } from "formik";
@@ -10,20 +9,25 @@ import CredentialInput from "./CredentialInput";
 import storageManager from "../utils/storageManager";
 
 const LoginPage = () => {
-  const emailIdInput = useRef(null);
-  const passwordInput = useRef(null);
-  const loginForm = useRef(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
   const [isNewAccount, setIsNewAccount] = useState(false);
 
   const getUniqueId = () => {
     const uniqueId = uuid();
     return uniqueId;
   };
+
+  const signinValidationSchema = Yup.object().shape({
+    email: Yup.string()
+      .matches(
+        /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+        "*Invalid email address"
+      )
+      .required("*Please fill in this field"),
+    password: Yup.string().required("*Please fill in this field"),
+  });
 
   const signupValidationSchema = Yup.object().shape({
     email: Yup.string()
@@ -38,62 +42,23 @@ const LoginPage = () => {
     dob: Yup.string().required("*Please fill in this field"),
   });
 
-  const checkEmailValidity = () => {
-    const emailValue = emailIdInput.current.value.trim();
-    const isEmailValid =
-      /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/.test(
-        emailValue
-      );
-
-    if (emailValue !== "") {
-      if (!isEmailValid) {
-        setEmailError("*Please enter valid email.");
-      } else {
-        setEmailError("");
-      }
-    } else {
-      setEmailError("*Please fill in this field ");
-    }
-
-    return isEmailValid;
-  };
-
-  const checkPasswordValidity = () => {
-    const passwordValue = passwordInput.current.value.trim();
-    const isPasswordValid = passwordValue.length > 0;
-
-    if (!isPasswordValid) {
-      setPasswordError("*Please fill in this field");
-    } else {
-      setPasswordError("");
-    }
-
-    return isPasswordValid;
-  };
-
-  const checkValidUser = (e) => {
-    e.preventDefault();
+  const checkValidUser = (values) => {
     let validUser = false;
 
-    checkEmailValidity();
-    checkPasswordValidity();
+    const database = storageManager.loadFromLocalStorage("usersDb");
 
-    if (checkEmailValidity() && checkPasswordValidity()) {
-      credentialsDb.forEach((user) => {
-        if (
-          user.emailId === emailIdInput.current.value &&
-          user.password === passwordInput.current.value
-        ) {
-          validUser = true;
-          dispatch(setUser(user.id));
-          navigate("/dashboard");
-          return;
-        }
-      });
-      validUser ? console.log("Login success") : console.log("Login failed");
-
-      loginForm.current.reset();
-    }
+    database.forEach((user) => {
+      if (
+        user.credentials.email === values.email &&
+        user.credentials.password === values.password
+      ) {
+        validUser = true;
+        dispatch(setUser(user.id));
+        navigate("/dashboard");
+        return;
+      }
+    });
+    validUser ? console.log("Login success") : console.log("Login failed");
   };
 
   const handleIsNewAccount = () => {
@@ -188,61 +153,33 @@ const LoginPage = () => {
             </>
           ) : (
             <>
-              <form
-                className="flex flex-col items-center gap-14"
-                ref={loginForm}
-                onSubmit={(e) => checkValidUser(e)}
-                id="loginForm"
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validationSchema={signinValidationSchema}
+                onSubmit={(values) => {
+                  checkValidUser(values);
+                }}
               >
-                <div className="relative flex flex-col gap-1">
-                  <label className="text-xs font-semibold text-neutral-600">
-                    EMAIL
-                  </label>
-                  <input
-                    className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
-                    ref={emailIdInput}
-                    type="email"
-                    onBlur={() => {
-                      checkEmailValidity();
-                    }}
-                    onFocus={() => {
-                      setEmailError("");
-                    }}
-                  ></input>
-                  {emailError && (
-                    <p className="absolute -bottom-5 text-sm font-medium text-red-600">
-                      {emailError}
-                    </p>
-                  )}
-                </div>
-                <div className="relative flex flex-col gap-1">
-                  <label className="text-xs font-semibold text-neutral-600">
-                    PASSWORD
-                  </label>
-                  <input
-                    className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
-                    ref={passwordInput}
-                    type="password"
-                    onBlur={() => {
-                      checkPasswordValidity();
-                    }}
-                    onFocus={() => {
-                      setPasswordError("");
-                    }}
-                  ></input>
-                  {passwordError && (
-                    <p className="absolute -bottom-5 text-sm font-medium text-red-600">
-                      {passwordError}
-                    </p>
-                  )}
-                </div>
-                <button
-                  className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
-                  type="submit"
-                >
-                  SIGN IN
-                </button>
-              </form>
+                {(formik) => (
+                  <form
+                    onSubmit={formik.handleSubmit}
+                    className="flex flex-col items-center gap-7"
+                  >
+                    <CredentialInput name="email" type="email" label="EMAIL" />
+                    <CredentialInput
+                      name="password"
+                      type="password"
+                      label="PASSWORD"
+                    />
+                    <button
+                      className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
+                      type="submit"
+                    >
+                      SIGN IN
+                    </button>
+                  </form>
+                )}
+              </Formik>
               <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
                 <p className="text-neutral-700">Don't have an account?</p>
                 <p
