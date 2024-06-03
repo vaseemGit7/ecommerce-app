417634fcea696f39487dedb0c04d0643e218bdd3
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-03 15:15:15 +0530

    Implement functionality to toggle between Sign in and Sign up

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -12,6 +12,7 @@ const LoginPage = () => {
 
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [isNewAccount, setIsNewAccount] = useState(false);
 
   const checkEmailValidity = () => {
     const emailValue = emailIdInput.current.value.trim();
@@ -71,6 +72,10 @@ const LoginPage = () => {
     }
   };
 
+  const handleIsNewAccount = () => {
+    setIsNewAccount((prevState) => !prevState);
+  };
+
   return (
     <div
       className="h-full grid place-items-center"
@@ -89,62 +94,88 @@ const LoginPage = () => {
           className="m-3 bg-cover bg-center rounded-lg"
           style={{ backgroundImage: "url('./src/assets/login-bg-image.jpeg')" }}
         ></div>
-        <div className=" mx-12 my-24 py-16 " id="loginContainer">
-          <form
-            className="flex flex-col items-center gap-14"
-            ref={loginForm}
-            onSubmit={(e) => checkValidUser(e)}
-            id="loginForm"
-          >
-            <div className="relative flex flex-col gap-1">
-              <label className="text-xs font-semibold text-neutral-600">
-                EMAIL
-              </label>
-              <input
-                className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
-                ref={emailIdInput}
-                type="email"
-                onBlur={() => {
-                  checkEmailValidity();
-                }}
-                onFocus={() => {
-                  setEmailError("");
-                }}
-              ></input>
-              {emailError && (
-                <p className="absolute -bottom-5 text-sm font-medium text-red-600">
-                  {emailError}
+        <div className=" mx-12 my-24 py-16 ">
+          {isNewAccount ? (
+            <>
+              Sign Up
+              <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
+                <p className="text-neutral-700">Already have an account?</p>
+                <p
+                  className="hover:text-violet-900 hover:font-medium cursor-pointer"
+                  onClick={handleIsNewAccount}
+                >
+                  Sign In
                 </p>
-              )}
-            </div>
-            <div className="relative flex flex-col gap-1">
-              <label className="text-xs font-semibold text-neutral-600">
-                PASSWORD
-              </label>
-              <input
-                className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
-                ref={passwordInput}
-                type="password"
-                onBlur={() => {
-                  checkPasswordValidity();
-                }}
-                onFocus={() => {
-                  setPasswordError("");
-                }}
-              ></input>
-              {passwordError && (
-                <p className="absolute -bottom-5 text-sm font-medium text-red-600">
-                  {passwordError}
+              </div>
+            </>
+          ) : (
+            <>
+              <form
+                className="flex flex-col items-center gap-14"
+                ref={loginForm}
+                onSubmit={(e) => checkValidUser(e)}
+                id="loginForm"
+              >
+                <div className="relative flex flex-col gap-1">
+                  <label className="text-xs font-semibold text-neutral-600">
+                    EMAIL
+                  </label>
+                  <input
+                    className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
+                    ref={emailIdInput}
+                    type="email"
+                    onBlur={() => {
+                      checkEmailValidity();
+                    }}
+                    onFocus={() => {
+                      setEmailError("");
+                    }}
+                  ></input>
+                  {emailError && (
+                    <p className="absolute -bottom-5 text-sm font-medium text-red-600">
+                      {emailError}
+                    </p>
+                  )}
+                </div>
+                <div className="relative flex flex-col gap-1">
+                  <label className="text-xs font-semibold text-neutral-600">
+                    PASSWORD
+                  </label>
+                  <input
+                    className="w-96 px-2 py-2 text-base bg-gray-100/75 rounded-md drop-shadow focus:outline focus:outline-neutral-500"
+                    ref={passwordInput}
+                    type="password"
+                    onBlur={() => {
+                      checkPasswordValidity();
+                    }}
+                    onFocus={() => {
+                      setPasswordError("");
+                    }}
+                  ></input>
+                  {passwordError && (
+                    <p className="absolute -bottom-5 text-sm font-medium text-red-600">
+                      {passwordError}
+                    </p>
+                  )}
+                </div>
+                <button
+                  className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
+                  type="submit"
+                >
+                  SIGN IN
+                </button>
+              </form>
+              <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
+                <p className="text-neutral-700">Don't have an account?</p>
+                <p
+                  className="hover:text-violet-900 hover:font-medium cursor-pointer"
+                  onClick={handleIsNewAccount}
+                >
+                  Sign Up
                 </p>
-              )}
-            </div>
-            <button
-              className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
-              type="submit"
-            >
-              SIGN IN
-            </button>
-          </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
