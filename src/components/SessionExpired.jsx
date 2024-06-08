9cc355b56b55ae6838d0a98d32cb21dd6a5b478c
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-08 21:51:07 +0530

    Create Session expired component

---
+++ src/components/SessionExpired.jsx
@@ -0,0 +1,18 @@
+const SessionExpired = () => {
+  return (
+    <div className="h-full grid place-items-center bg-neutral-300">
+      <div className="flex flex-col gap-2 items-center p-4 text-base bg-neutral-50 font-normal drop-shadow-md rounded-md">
+        <p className="text-xl font-semibold">Session expired</p>
+        <div className="text-center">
+          <p>Your session has expired.</p>
+          <p>You will be redirected to the Login page.</p>
+        </div>
+        <button className="w-4/5 mt-2 p-2 py-1 text-lg bg-neutral-800 text-neutral-50 font-medium drop-shadow rounded hover:scale-[1.02]">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SessionExpired;
