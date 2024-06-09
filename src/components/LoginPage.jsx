d970fb6d0684b435bf8956cd1e92501ce692b6ba
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-09 21:52:40 +0530

    Refactor login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -13,6 +13,7 @@ const LoginPage = () => {
   const dispatch = useDispatch();
 
   const [isNewAccount, setIsNewAccount] = useState(false);
+  const [validUser, setValidUser] = useState(null);
 
   const getUniqueId = () => {
     const uniqueId = uuid();
@@ -30,21 +31,19 @@ const LoginPage = () => {
   });
 
   const signupValidationSchema = Yup.object().shape({
-    email: Yup.string()
+    newEmail: Yup.string()
       .matches(
         /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
         "*Invalid email address"
       )
       .required("*Please fill in this field"),
-    password: Yup.string()
+    newPassword: Yup.string()
       .min(8, "*Must be atleast 8 characters")
       .required("*Please fill in this field"),
-    dob: Yup.string().required("*Please fill in this field"),
+    newDob: Yup.string().required("*Please fill in this field"),
   });
 
   const checkValidUser = (values) => {
-    let validUser = false;
-
     const database = storageManager.loadFromLocalStorage("usersDb");
 
     database.forEach((user) => {
@@ -52,31 +51,33 @@ const LoginPage = () => {
         user.credentials.email === values.email &&
         user.credentials.password === values.password
       ) {
-        validUser = true;
+        setValidUser(true);
         dispatch(setUser(user));
         navigate("/dashboard");
         return;
+      } else {
+        setValidUser(false);
       }
     });
-    validUser ? console.log("Login success") : console.log("Login failed");
   };
 
   const handleIsNewAccount = () => {
     setIsNewAccount((prevState) => !prevState);
+    setValidUser(true);
   };
 
   const handleAddNewUser = (values) => {
     const credentials = {
-      email: values.email,
-      password: values.password,
+      email: values.newEmail,
+      password: values.newPassword,
     };
 
     const newUser = {
       id: getUniqueId(),
       credentials: credentials,
       userDetails: {
-        email: values.email,
-        dateOfBirth: values.dob,
+        email: values.newEmail,
+        dateOfBirth: values.newDob,
       },
     };
 
@@ -96,16 +97,20 @@ const LoginPage = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className="z-10 grid grid-cols-2 p-2 bg-neutral-200/75 bg-opa rounded-xl drop-shadow-lg">
+      <div className="z-10 w-4/6 h-4/5 grid grid-cols-2 p-2 bg-neutral-200/75 bg-opa rounded-xl drop-shadow-lg">
         <div
           className="m-3 bg-cover bg-center rounded-lg"
           style={{ backgroundImage: "url('./src/assets/login-card.jpeg')" }}
         ></div>
-        <div className=" mx-12 my-24 py-16 ">
+        <div className="grid grid-rows-[max-content_1fr]">
+          <img
+            className="justify-self-center mt-5"
+            src="./src/assets/logo.svg"
+          />
           {isNewAccount ? (
             <>
               <Formik
-                initialValues={{ email: "", password: "", dob: "" }}
+                initialValues={{ newEmail: "", newPassword: "", newDob: "" }}
                 validationSchema={signupValidationSchema}
                 onSubmit={(values) => {
                   alert(JSON.stringify(values, null, 2));
@@ -113,39 +118,47 @@ const LoginPage = () => {
                 }}
               >
                 {(formik) => (
-                  <form
-                    onSubmit={formik.handleSubmit}
-                    className="flex flex-col items-center gap-7"
-                  >
-                    <CredentialInput name="email" type="email" label="EMAIL" />
-                    <CredentialInput
-                      name="password"
-                      type="password"
-                      label="PASSWORD"
-                    />
-                    <CredentialInput
-                      name="dob"
-                      type="date"
-                      label="DATE OF BIRTH"
-                    />
-                    <button
-                      className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
-                      type="submit"
+                  <div className="place-self-center flex flex-col">
+                    <form
+                      onSubmit={formik.handleSubmit}
+                      className="flex flex-col items-center gap-7"
                     >
-                      SIGN UP
-                    </button>
-                  </form>
+                      <CredentialInput
+                        name="newEmail"
+                        type="email"
+                        label="EMAIL"
+                      />
+                      <CredentialInput
+                        name="newPassword"
+                        type="password"
+                        label="PASSWORD"
+                      />
+                      <CredentialInput
+                        name="newDob"
+                        type="date"
+                        label="DATE OF BIRTH"
+                      />
+                      <button
+                        className="w-1/2 px-8 py-2 text-md font-semibold  text-neutral-50 bg-neutral-800 drop-shadow-md rounded-md hover:bg-neutral-950"
+                        type="submit"
+                      >
+                        SIGN UP
+                      </button>
+                    </form>
+                    <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
+                      <p className="text-neutral-700">
+                        Already have an account?
+                      </p>
+                      <p
+                        className="hover:text-violet-900 hover:font-medium cursor-pointer"
+                        onClick={handleIsNewAccount}
+                      >
+                        Sign In
+                      </p>
+                    </div>
+                  </div>
                 )}
               </Formik>
-              <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
-                <p className="text-neutral-700">Already have an account?</p>
-                <p
-                  className="hover:text-violet-900 hover:font-medium cursor-pointer"
-                  onClick={handleIsNewAccount}
-                >
-                  Sign In
-                </p>
-              </div>
             </>
           ) : (
             <>
@@ -157,34 +170,47 @@ const LoginPage = () => {
                 }}
               >
                 {(formik) => (
-                  <form
-                    onSubmit={formik.handleSubmit}
-                    className="flex flex-col items-center gap-7"
-                  >
-                    <CredentialInput name="email" type="email" label="EMAIL" />
-                    <CredentialInput
-                      name="password"
-                      type="password"
-                      label="PASSWORD"
-                    />
-                    <button
-                      className="w-32 px-8 py-2 text-md font-semibold  text-neutral-50 bg-violet-900 drop-shadow-md rounded-md hover:bg-violet-950"
-                      type="submit"
+                  <div className="relative place-self-center flex flex-col">
+                    <div className="absolute -top-7 text-red-600 text-bases font-medium text-center">
+                      {validUser === false ? (
+                        <p className="">Invalid Email or Password</p>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                    <form
+                      onSubmit={formik.handleSubmit}
+                      className="relative place-self-center flex flex-col items-center gap-7"
                     >
-                      SIGN IN
-                    </button>
-                  </form>
+                      <CredentialInput
+                        name="email"
+                        type="email"
+                        label="EMAIL"
+                      />
+                      <CredentialInput
+                        name="password"
+                        type="password"
+                        label="PASSWORD"
+                      />
+                      <button
+                        className="w-1/2 px-8 py-2 text-md font-semibold  text-neutral-50 bg-neutral-800 drop-shadow-md rounded-md hover:bg-neutral-950"
+                        type="submit"
+                      >
+                        SIGN IN
+                      </button>
+                    </form>
+                    <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
+                      <p className="text-neutral-700">Don't have an account?</p>
+                      <p
+                        className="hover:text-violet-900 hover:font-medium cursor-pointer"
+                        onClick={handleIsNewAccount}
+                      >
+                        Sign Up
+                      </p>
+                    </div>
+                  </div>
                 )}
               </Formik>
-              <div className="flex gap-1 mt-4 text-sm font-normal justify-end">
-                <p className="text-neutral-700">Don't have an account?</p>
-                <p
-                  className="hover:text-violet-900 hover:font-medium cursor-pointer"
-                  onClick={handleIsNewAccount}
-                >
-                  Sign Up
-                </p>
-              </div>
             </>
           )}
         </div>
