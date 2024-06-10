b92186c266e64041ebe3003c799f1bab9f2a9deb
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-10 16:21:27 +0530

    Add body section to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,13 +12,14 @@ const Home = () => {
   };
 
   const carouselDots = [];
+  const curratedItems = ["Men", "Women", "Kids", "Baby"];
 
   for (let i = 1; i <= 5; i++) {
     carouselDots.push(
       <div
         key={i}
         className={`rounded-full bg-neutral-50 ${
-          index === i ? "h-3 w-3" : "h-2 w-2"
+          index === i ? "h-3 w-3" : "h-2 w-2 bg-neutral-400"
         }`}
       ></div>
     );
@@ -33,9 +34,9 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="h-full flex flex-col">
+    <div className="flex flex-col gap-4">
       <div
-        className="relative h-5/6 w-full rounded"
+        className="relative h-[35rem] w-full rounded"
         style={{
           backgroundImage: `url('../src/assets/banners/carousel-banner-${index}.jpg`,
           backgroundPosition: "center",
@@ -48,6 +49,74 @@ const Home = () => {
         </p>
         <div className="absolute z-1 flex bottom-3 right-1/2 items-center gap-1">
           {carouselDots}
+        </div>
+      </div>
+      <div className="mt-2 p-4 flex justify-evenly">
+        <div className="w-1/4 p-4 flex flex-col items-start gap-1 outline outline-1 outline-neutral-300 bg-neutral-200/50">
+          <p className="text-lg font-semibold text-neutral-900">
+            Quality Products
+          </p>
+          <p className="text-sm font-normal text-neutral-600">
+            Each garment crafted with premium materials and attention to detail.
+          </p>
+        </div>
+        <div className="w-1/4 p-4 flex flex-col items-start gap-1 outline outline-1 outline-neutral-300 bg-neutral-200/50">
+          <p className="text-lg font-semibold text-neutral-900">
+            New Arrival Everyday
+          </p>
+          <p className="text-sm font-normal text-neutral-600">
+            We introduce fresh, stylish clothing daily to keep your wardrobe
+            vibrant and on-trend.
+          </p>
+        </div>
+        <div className="w-1/4 p-4 flex flex-col items-start gap-1 outline outline-1 outline-neutral-300 bg-neutral-200/50">
+          <p className="text-lg font-semibold text-neutral-900">
+            Fast & Free Shipping
+          </p>
+          <p className="text-sm font-normal text-neutral-600">
+            We ensure your purchases arrive swiftly and safely, straight to your
+            doorstep.
+          </p>
+        </div>
+      </div>
+      <div className="p-7">
+        <p className="text-2xl font-semibold text-center text-neutral-800 mb-10">
+          CURRATED PICKS
+        </p>
+        <div className="flex justify-evenly">
+          {curratedItems.map((item, i) => (
+            <div
+              key={i}
+              className="h-72 w-72 flex p-3 rounded justify-center"
+              style={{
+                backgroundImage: `url('../src/assets/cards/card-${item}.jpeg`,
+                backgroundSize: "cover",
+              }}
+            >
+              <button className="self-end px-4 py-1 bg-neutral-50/90 text-base font-medium text-neutral-800 rounded">
+                Shop {item}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-2/3  m-4 self-center grid grid-cols-2 rounded">
+        <div
+          className="rounded-l"
+          style={{
+            backgroundImage: `url('../src/assets/cards/offer-card.jpg`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div className="flex flex-col gap-2 px-3 py-6 bg-neutral-800 text-neutral-50 rounded-r">
+          <p className="text-base font-medium">LIMITED OFFER</p>
+          <p className="text-4xl text-balance font-medium">
+            40% off on purchases above ₹5000
+          </p>
+          <button className="w-1/3 px-4 py-1 bg-neutral-50 text-base font-medium text-neutral-800 rounded">
+            Grab it now!
+          </button>
         </div>
       </div>
     </div>
