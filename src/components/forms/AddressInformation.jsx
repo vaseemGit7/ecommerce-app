dea8241859aa8acbaecce3c0f1069c6120fe2165
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-13 10:49:45 +0530

    Refactor AddressInformation component

--- src/components/forms/AddressInformation.jsx
+++ src/components/forms/AddressInformation.jsx
@@ -3,14 +3,8 @@ import * as Yup from "yup";
 import { v4 as uuid } from "uuid";
 import DataInput from "./DataInput";
 import storageManager from "../../utils/storageManager";
-import { IonIcon } from "@ionic/react";
-import { createOutline } from "ionicons/icons";
 
-const AddressInformation = ({
-  userData,
-  sectionVisibility,
-  handleSectionVisibility,
-}) => {
+const AddressInformation = ({ userData, handleSectionVisibility }) => {
   const database = storageManager.loadFromLocalStorage("usersDb");
   const userDB = database.find((user) => user.id === userData.id);
   const userAddress = userDB?.userDetails?.addresses?.[0];
@@ -52,79 +46,59 @@ const AddressInformation = ({
 
   return (
     <>
-      {sectionVisibility["addressInformation"] && userAddress?.streetAddress ? (
-        <div className="flex justify-between bg-neutral-100 outline outline-1 outline-neutral-200  p-2 rounded">
-          <div className="flex flex-col gap-1 text-sm font-medium text-neutral-800">
-            <p>{userDB.userDetails.fullName}</p>
-            <p className="mb-1">{userDB.userDetails.phoneNumber}</p>
-            <p>{userAddress.flatAddress}</p>
-            <p>{userAddress.streetAddress}</p>
-            <p>{userAddress.town}</p>
-            <p>{userAddress.state}</p>
-            <p>{userAddress.pincode}</p>
-          </div>
-          <p
-            className="px-1  bg-neutral-50 self-start text-neutral-800 cursor-pointer rounded"
-            onClick={() => handleSectionVisibility("addressInformation")}
+      <Formik
+        initialValues={{
+          streetAddress: userAddress?.streetAddress || "",
+          flatAddress: userAddress?.flatAddress || "",
+          town: userAddress?.town || "",
+          pincode: userAddress?.pincode || "",
+          state: userAddress?.state || "",
+        }}
+        validationSchema={addressInfoValidationSchema}
+        onSubmit={(values) => handleSubmission(values)}
+      >
+        {(formik) => (
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex flex-col  justify-start gap-2"
           >
-            <IonIcon icon={createOutline} className="text-xl" />
-          </p>
-        </div>
-      ) : (
-        <Formik
-          initialValues={{
-            streetAddress: userAddress?.streetAddress || "",
-            flatAddress: userAddress?.flatAddress || "",
-            town: userAddress?.town || "",
-            pincode: userAddress?.pincode || "",
-            state: userAddress?.state || "",
-          }}
-          validationSchema={addressInfoValidationSchema}
-          onSubmit={(values) => handleSubmission(values)}
-        >
-          {(formik) => (
-            <form
-              onSubmit={formik.handleSubmit}
-              className="flex flex-col  justify-start gap-2"
+            <div className="flex flex-col gap-1">
+              <p className="text-sm font-medium text-neutral-800">Name</p>
+              <p className="text-sm font-medium text-neutral-800">
+                {userDB.userDetails.fullName}
+              </p>
+            </div>
+            <DataInput
+              name="streetAddress"
+              type="text"
+              label="Street Address"
+            />
+            <DataInput
+              name="flatAddress"
+              type="text"
+              label="Flat No. / House name"
+            />
+            <div className="flex justify-between">
+              <DataInput name="town" type="text" label="Town / City" />
+              <DataInput name="pincode" type="number" label="Pincode" />
+            </div>
+            <DataInput name="state" type="text" label="State" />
+            <button
+              type="submit"
+              className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded hover:drop-shadow-lg"
             >
-              <div className="flex flex-col gap-1">
-                <p className="text-sm font-medium text-neutral-800">Name</p>
-                <p className="text-sm font-medium text-neutral-800">
-                  {userDB.userDetails.fullName}
-                </p>
-              </div>
-              <DataInput
-                name="streetAddress"
-                type="text"
-                label="Street Address"
-              />
-              <DataInput
-                name="flatAddress"
-                type="text"
-                label="Flat No. / House name"
-              />
-              <div className="flex justify-between">
-                <DataInput name="town" type="text" label="Town / City" />
-                <DataInput name="pincode" type="number" label="Pincode" />
-              </div>
-              <DataInput name="state" type="text" label="State" />
-              <button
-                type="submit"
-                className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded hover:drop-shadow-lg"
-              >
-                Save
-              </button>
-              <button
-                type="button"
-                className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-50 outline outline-1 outline-neutral-600 text-base text-neutral-800 font-normal rounded hover:drop-shadow-lg"
-                onClick={() => handleSectionVisibility("addressInformation")}
-              >
-                Cancel
-              </button>
-            </form>
-          )}
-        </Formik>
-      )}
+              Save
+            </button>
+            <button
+              type="button"
+              className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-50 outline outline-1 outline-neutral-600 text-base text-neutral-800 font-normal rounded hover:drop-shadow-lg"
+              onClick={() => handleSectionVisibility("addressInformation")}
+            >
+              Cancel
+            </button>
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
