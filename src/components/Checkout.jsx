87507ee9f359ff29f3edfba70624a8f588ce8ece
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-06-13 11:25:41 +0530

    Add navigation back functionality

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import UserInformation from "./forms/UserInformation";
 import AddressInformation from "./forms/AddressInformation";
@@ -8,7 +8,7 @@ import { getCurrentDate } from "../utils/dateHandler";
 import { removeAllProductFromCart } from "../actions/userActions";
 import ConfirmModal from "./ConfirmModal";
 import { IonIcon } from "@ionic/react";
-import { createOutline } from "ionicons/icons";
+import { arrowBackOutline, createOutline } from "ionicons/icons";
 
 const Checkout = () => {
   const location = useLocation();
@@ -16,6 +16,7 @@ const Checkout = () => {
   const userData = useSelector((state) => state.userReducer);
   const cartProduct = useSelector((state) => state.cartReducer);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [sectionVisibility, setSectionVisiblity] = useState({
     userInformation: true,
     addressInformation: true,
@@ -74,157 +75,170 @@ const Checkout = () => {
   };
 
   return (
-    <div className="mx-28">
-      <div className="grid grid-cols-[3fr_2fr] ">
-        <div className="flex flex-col p-6 gap-3">
-          <div>
-            <p className="text-2xl font-bold"> Checkout</p>
-          </div>
-          <div className="p-3">
-            <p className="text-lg font-medium mb-4">My information</p>
-            {sectionVisibility.userInformation && userDetails.fullName ? (
-              <div className="flex justify-between bg-neutral-100 outline outline-1 outline-neutral-200 p-2 rounded">
-                <div className="flex flex-col gap-1">
-                  <p className="text-sm font-medium text-neutral-800">
-                    {userDetails.fullName}
-                  </p>
-                  <p className="text-sm font-medium text-neutral-800">
-                    {userDetails.email}
+    <>
+      <div
+        className="absolute top-20 left-4 flex gap-1 items-center"
+        onClick={() => navigate(-1)}
+      >
+        <IonIcon icon={arrowBackOutline} className="text-lg" />
+        <p className="text-lg text-neutral-800 font-medium">Cart</p>
+      </div>
+      <div className="mx-28">
+        <div className="grid grid-cols-[3fr_2fr] ">
+          <div className="flex flex-col p-6 gap-3">
+            <div>
+              <p className="text-2xl font-bold"> Checkout</p>
+            </div>
+            <div className="p-3">
+              <p className="text-lg font-medium mb-4">My information</p>
+              {sectionVisibility.userInformation && userDetails.fullName ? (
+                <div className="flex justify-between bg-neutral-100 outline outline-1 outline-neutral-200 p-2 rounded">
+                  <div className="flex flex-col gap-1">
+                    <p className="text-sm font-medium text-neutral-800">
+                      {userDetails.fullName}
+                    </p>
+                    <p className="text-sm font-medium text-neutral-800">
+                      {userDetails.email}
+                    </p>
+                  </div>
+                  <p
+                    className="px-1  bg-neutral-50 self-start text-neutral-800 cursor-pointer rounded"
+                    onClick={() => handleSectionVisibility("userInformation")}
+                  >
+                    <IonIcon icon={createOutline} className="text-xl" />
                   </p>
                 </div>
-                <p
-                  className="px-1  bg-neutral-50 self-start text-neutral-800 cursor-pointer rounded"
-                  onClick={() => handleSectionVisibility("userInformation")}
-                >
-                  <IonIcon icon={createOutline} className="text-xl" />
-                </p>
-              </div>
-            ) : (
-              <UserInformation
-                userData={userData}
-                sectionVisibility={sectionVisibility}
-                handleSectionVisibility={handleSectionVisibility}
-              />
-            )}
-          </div>
-          <div className="p-3">
-            <p className="text-lg font-medium mb-4">Billing address</p>
-            {sectionVisibility["addressInformation"] &&
-            userAddress?.streetAddress ? (
-              <div className="flex justify-between bg-neutral-100 outline outline-1 outline-neutral-200  p-2 rounded">
-                <div className="flex flex-col gap-1 text-sm font-medium text-neutral-800">
-                  <p>{userDB.userDetails.fullName}</p>
-                  <p className="mb-1">{userDB.userDetails.phoneNumber}</p>
-                  <p>{userAddress.flatAddress}</p>
-                  <p>{userAddress.streetAddress}</p>
-                  <p>{userAddress.town}</p>
-                  <p>{userAddress.state}</p>
-                  <p>{userAddress.pincode}</p>
+              ) : (
+                <UserInformation
+                  userData={userData}
+                  sectionVisibility={sectionVisibility}
+                  handleSectionVisibility={handleSectionVisibility}
+                />
+              )}
+            </div>
+            <div className="p-3">
+              <p className="text-lg font-medium mb-4">Billing address</p>
+              {sectionVisibility["addressInformation"] &&
+              userAddress?.streetAddress ? (
+                <div className="flex justify-between bg-neutral-100 outline outline-1 outline-neutral-200  p-2 rounded">
+                  <div className="flex flex-col gap-1 text-sm font-medium text-neutral-800">
+                    <p>{userDB.userDetails.fullName}</p>
+                    <p className="mb-1">{userDB.userDetails.phoneNumber}</p>
+                    <p>{userAddress.flatAddress}</p>
+                    <p>{userAddress.streetAddress}</p>
+                    <p>{userAddress.town}</p>
+                    <p>{userAddress.state}</p>
+                    <p>{userAddress.pincode}</p>
+                  </div>
+                  <p
+                    className="px-1  bg-neutral-50 self-start text-neutral-800 cursor-pointer rounded"
+                    onClick={() =>
+                      handleSectionVisibility("addressInformation")
+                    }
+                  >
+                    <IonIcon icon={createOutline} className="text-xl" />
+                  </p>
                 </div>
-                <p
-                  className="px-1  bg-neutral-50 self-start text-neutral-800 cursor-pointer rounded"
-                  onClick={() => handleSectionVisibility("addressInformation")}
-                >
-                  <IonIcon icon={createOutline} className="text-xl" />
-                </p>
-              </div>
-            ) : (
-              <AddressInformation
-                userData={userData}
-                sectionVisibility={sectionVisibility}
-                handleSectionVisibility={handleSectionVisibility}
-              />
-            )}
-          </div>
-          <div className="p-3">
-            <p className="text-lg font-medium mb-4">View order details</p>
-            <div className="flex gap-2">
-              {cartProduct.map((product) => (
-                <div
-                  key={product.id}
-                  className="grid  w-44 grid-rows-[max-content_1fr] text-neutral-800 outline outline-1 outline-neutral-600 rounded p-2"
-                >
-                  <img
-                    className="place-self-center h-40 rounded"
-                    src={product.image}
-                  />
-                  <div className="grid grid-rows-[max-content_1fr] gap-1">
-                    <p className="text-sm font-medium">{product.name}</p>
-                    <div className="self-end">
-                      <p className="text-sm font-medium">₹ {product.price}</p>
-                      <div className="flex gap-2">
-                        <p className="text-sm font-normal text-neutral-600">
-                          Size:
-                        </p>
-                        <p className=" text-sm font-medium">{product.size}</p>
-                      </div>
-                      <div className="flex gap-2">
-                        <p className="text-sm font-normal text-neutral-600">
-                          Color:
-                        </p>
-                        <p className=" text-sm font-medium">{product.color}</p>
-                      </div>
-                      <div className="flex gap-2">
-                        <p className="text-sm font-normal text-neutral-600">
-                          Total:
-                        </p>
-                        <p className=" text-sm font-medium">
-                          ₹ {product.price * product.quantity}
-                        </p>
-                      </div>
-                      <div className="flex gap-2">
-                        <p className="text-sm font-normal text-neutral-600">
-                          Quantity:
-                        </p>
-                        <p className=" text-sm font-medium">
-                          {product.quantity}
-                        </p>
+              ) : (
+                <AddressInformation
+                  userData={userData}
+                  sectionVisibility={sectionVisibility}
+                  handleSectionVisibility={handleSectionVisibility}
+                />
+              )}
+            </div>
+            <div className="p-3">
+              <p className="text-lg font-medium mb-4">View order details</p>
+              <div className="flex gap-2">
+                {cartProduct.map((product) => (
+                  <div
+                    key={product.id}
+                    className="grid  w-44 grid-rows-[max-content_1fr] text-neutral-800 outline outline-1 outline-neutral-600 rounded p-2"
+                  >
+                    <img
+                      className="place-self-center h-40 rounded"
+                      src={product.image}
+                    />
+                    <div className="grid grid-rows-[max-content_1fr] gap-1">
+                      <p className="text-sm font-medium">{product.name}</p>
+                      <div className="self-end">
+                        <p className="text-sm font-medium">₹ {product.price}</p>
+                        <div className="flex gap-2">
+                          <p className="text-sm font-normal text-neutral-600">
+                            Size:
+                          </p>
+                          <p className=" text-sm font-medium">{product.size}</p>
+                        </div>
+                        <div className="flex gap-2">
+                          <p className="text-sm font-normal text-neutral-600">
+                            Color:
+                          </p>
+                          <p className=" text-sm font-medium">
+                            {product.color}
+                          </p>
+                        </div>
+                        <div className="flex gap-2">
+                          <p className="text-sm font-normal text-neutral-600">
+                            Total:
+                          </p>
+                          <p className=" text-sm font-medium">
+                            ₹ {product.price * product.quantity}
+                          </p>
+                        </div>
+                        <div className="flex gap-2">
+                          <p className="text-sm font-normal text-neutral-600">
+                            Quantity:
+                          </p>
+                          <p className=" text-sm font-medium">
+                            {product.quantity}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mt-2 p-3">
+            <div className="flex flex-col bg-neutral-50 text-neutral-800 outline outline-1 outline-neutral-200 p-3 rounded-md">
+              <div className="flex justify-between pb-3 border-b-2 border-neutral-400">
+                <div>
+                  <p className=" text-base font-normal">Order value</p>
+                  <p className=" text-base font-normal">Delivery charges</p>
                 </div>
-              ))}
+                <div>
+                  <p className=" text-base font-normal">Rs. {totalPrice}</p>
+                  <p className=" text-base font-normal">
+                    {deliveryCharge === 0 ? "Free" : `Rs. ${deliveryCharge}`}
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-between items-center pt-3">
+                <p className="text-xl font-medium">Total charges</p>
+                <p className="text-xl font-medium">Rs. {totalPrice}</p>
+              </div>
+              <button
+                className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded hover:shadow-lg"
+                onClick={handlePlaceOrder}
+              >
+                Place Order
+              </button>
             </div>
           </div>
         </div>
-        <div className="mt-2 p-3">
-          <div className="flex flex-col bg-neutral-50 text-neutral-800 outline outline-1 outline-neutral-200 p-3 rounded-md">
-            <div className="flex justify-between pb-3 border-b-2 border-neutral-400">
-              <div>
-                <p className=" text-base font-normal">Order value</p>
-                <p className=" text-base font-normal">Delivery charges</p>
-              </div>
-              <div>
-                <p className=" text-base font-normal">Rs. {totalPrice}</p>
-                <p className=" text-base font-normal">
-                  {deliveryCharge === 0 ? "Free" : `Rs. ${deliveryCharge}`}
-                </p>
-              </div>
-            </div>
-            <div className="flex justify-between items-center pt-3">
-              <p className="text-xl font-medium">Total charges</p>
-              <p className="text-xl font-medium">Rs. {totalPrice}</p>
-            </div>
-            <button
-              className="py-2 px-3 self-center w-3/5 mt-3 text-center align-middle bg-neutral-800 text-base text-neutral-50 font-normal rounded hover:shadow-lg"
-              onClick={handlePlaceOrder}
-            >
-              Place Order
-            </button>
-          </div>
-        </div>
+        <ConfirmModal
+          dialogToggle={dialogToggle}
+          handleDialogToggle={handleDialogToggle}
+          userData={userData}
+          orderDetails={orderDetails}
+          deliveryCharge={deliveryCharge}
+          totalPrice={totalPrice}
+          formattedDate={formattedDate}
+          invoiceNo={invoiceNo}
+        />
       </div>
-      <ConfirmModal
-        dialogToggle={dialogToggle}
-        handleDialogToggle={handleDialogToggle}
-        userData={userData}
-        orderDetails={orderDetails}
-        deliveryCharge={deliveryCharge}
-        totalPrice={totalPrice}
-        formattedDate={formattedDate}
-        invoiceNo={invoiceNo}
-      />
-    </div>
+    </>
   );
 };
 
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getProductDetail } from "../api/API";
 import { useDispatch } from "react-redux";
 import { toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { addProductToCart } from "../actions/userActions";
 import DetailLoading from "./DetailLoading";
+import { IonIcon } from "@ionic/react";
+import { arrowBackOutline } from "ionicons/icons";
 
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
@@ -16,6 +18,7 @@ const ProductDetail = () => {
   const { id } = useParams();
   const hasFetched = useRef(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   let productData = undefined;
 
   const handleVariantSelection = (code) => {
@@ -81,79 +84,90 @@ const ProductDetail = () => {
   console.log(currentVariant);
 
   return (
-    <div className={`grid mx-28 ${loading ? "h-screen" : "justify-center"}`}>
-      {loading ? (
-        <DetailLoading />
-      ) : (
-        <div className="grid grid-cols-2 p-7 justify-items-center bg-neutral-50 outline outline-1 outline-neutral-200  text-neutral-800 rounded">
-          <div className="w-5/6">
-            <img
-              className="rounded"
-              src={currentVariant?.galleryDetails[0]?.baseUrl}
-            />
-          </div>
-          <div className="p-5 flex flex-col gap-2 overflow-y-scroll">
-            <div>
-              <p className="text-xl font-semibold">{product.name}</p>
-              <p className="text-lg font-medium">₹{product.whitePrice.price}</p>
+    <>
+      <div
+        className="absolute top-20 left-4 flex gap-1 items-center"
+        onClick={() => navigate(-1)}
+      >
+        <IonIcon icon={arrowBackOutline} className="text-lg" />
+        <p className="text-lg text-neutral-800 font-medium">Go back</p>
+      </div>
+      <div className={`grid mx-28 ${loading ? "h-screen" : "justify-center"}`}>
+        {loading ? (
+          <DetailLoading />
+        ) : (
+          <div className="grid grid-cols-2 p-7 justify-items-center bg-neutral-50 outline outline-1 outline-neutral-200  text-neutral-800 rounded">
+            <div className="w-5/6">
+              <img
+                className="rounded"
+                src={currentVariant?.galleryDetails[0]?.baseUrl}
+              />
             </div>
-            <p className="text-base font-medium text-neutral-700">
-              {currentVariant?.colourDescription}
-            </p>
-            <div className="flex h-60 gap-2 p-2 overflow-x-scroll">
-              {product &&
-                product.articlesList.map((item) => (
-                  <img
-                    key={item.code}
-                    src={item?.galleryDetails[0]?.baseUrl}
-                    className={`cursor-pointer outline outline-1 hover:outline-neutral-500 rounded ${
-                      currentVariant?.code === item.code
-                        ? "outline-neutral-700"
-                        : "outline-neutral-300"
-                    }`}
-                    onClick={() => handleVariantSelection(item.code)}
-                  />
-                ))}
-            </div>
-            <p className="text-base font-medium">SIZES</p>
-            {sizeError && (
-              <p className="text-sm text-red-600 font-normal">
-                *Please select a size
+            <div className="p-5 flex flex-col gap-2 overflow-y-scroll">
+              <div>
+                <p className="text-xl font-semibold">{product.name}</p>
+                <p className="text-lg font-medium">
+                  ₹{product.whitePrice.price}
+                </p>
+              </div>
+              <p className="text-base font-medium text-neutral-700">
+                {currentVariant?.colourDescription}
               </p>
-            )}
-            <div className="flex gap-2">
-              {currentVariant &&
-                currentVariant.variantsList.map((item) => (
-                  <div
-                    className={`p-2 w-1/5 text-center text-sm font-medium outline outline-1 outline-neutral-400 rounded-sm hover:scale-[1.04] hover:shadow-md cursor-pointer ${
-                      selectedSize === item.size.name
-                        ? "bg-neutral-300 scale-[1.04]"
-                        : ""
-                    }`}
-                    key={item.code}
-                    onClick={() => {
-                      setSelectedSize(item.size.name);
-                      setSizeError(false);
-                    }}
-                  >
-                    {item.size.name}
-                  </div>
-                ))}
-            </div>
-            <button
-              className="mt-2 py-2 px-3 w-3/5 text-center self-center text-base font-medium text-neutral-50 bg-neutral-700 rounded hover:bg-neutral-800"
-              onClick={addProduct}
-            >
-              Add
-            </button>
-            <div>
-              <p className="text-lg font-medium">DESCRIPTION</p>
-              <p className="text-sm font-normal">{product.description}</p>
+              <div className="flex h-60 gap-2 p-2 overflow-x-scroll">
+                {product &&
+                  product.articlesList.map((item) => (
+                    <img
+                      key={item.code}
+                      src={item?.galleryDetails[0]?.baseUrl}
+                      className={`cursor-pointer outline outline-1 hover:outline-neutral-500 rounded ${
+                        currentVariant?.code === item.code
+                          ? "outline-neutral-700"
+                          : "outline-neutral-300"
+                      }`}
+                      onClick={() => handleVariantSelection(item.code)}
+                    />
+                  ))}
+              </div>
+              <p className="text-base font-medium">SIZES</p>
+              {sizeError && (
+                <p className="text-sm text-red-600 font-normal">
+                  *Please select a size
+                </p>
+              )}
+              <div className="flex gap-2">
+                {currentVariant &&
+                  currentVariant.variantsList.map((item) => (
+                    <div
+                      className={`p-2 w-1/5 text-center text-sm font-medium outline outline-1 outline-neutral-400 rounded-sm hover:scale-[1.04] hover:shadow-md cursor-pointer ${
+                        selectedSize === item.size.name
+                          ? "bg-neutral-300 scale-[1.04]"
+                          : ""
+                      }`}
+                      key={item.code}
+                      onClick={() => {
+                        setSelectedSize(item.size.name);
+                        setSizeError(false);
+                      }}
+                    >
+                      {item.size.name}
+                    </div>
+                  ))}
+              </div>
+              <button
+                className="mt-2 py-2 px-3 w-3/5 text-center self-center text-base font-medium text-neutral-50 bg-neutral-700 rounded hover:bg-neutral-800"
+                onClick={addProduct}
+              >
+                Add
+              </button>
+              <div>
+                <p className="text-lg font-medium">DESCRIPTION</p>
+                <p className="text-sm font-normal">{product.description}</p>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
